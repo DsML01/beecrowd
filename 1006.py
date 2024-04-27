6612c5d6c3732bc8263a9e6b1c6fcb14e4e7b76b
davi <davimelovas61@gmail.com>
2024-04-27 19:21:50 -0300

    Adding some questions

---
+++ 1006.py
@@ -0,0 +1,11 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+pesoa = 2
+pesob = 3
+pesoc = 5
+
+media = ((a * pesoa) + (b * pesob) + (c * pesoc))/(pesoa + pesob + pesoc)
+
+print("MEDIA = {:.1f}".format(media))
---
+++ 1008.py
@@ -0,0 +1,7 @@
+numero = input()
+horas_trabalhadas = float(input())
+salario = float(input())
+
+print("NUMBER =", numero)
+print("SALARY = U$ {:.2f}".format(horas_trabalhadas * salario))
+
